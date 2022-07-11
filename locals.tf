b22b392a0318559607374da9f2581626c73018ea
Chris Funderburg <chris@cloudcauldron.io>
2022-07-11 23:30:12 +0100

    split out user_data, add more tools, get ready to template it

--- locals.tf
+++ locals.tf
@@ -1,18 +1,14 @@
-
 locals {
 
   name    = "chrisfu-support"
-  your_ip = "31.121.189.242"
+  your_ip = "188.223.70.12"
 
   vpc           = "vpc-041eae251015f69cc"
   subnet_id     = "subnet-0297c5f655e0c061e"
   instance_type = "t3.medium"
 
-  user_data = <<-EOT
-  #!/bin/bash
-  yum install -y postgresql nmap
-  yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-  EOT
+  # Todo: Template this w/ versions
+  user_data = file("${path.module}/user_data.sh")
 
   tags = {
     Owner       = "Chris Funderburg"
